185705548948b2edbcee00e92f60443a606a86c0
Abewd <116496332+abewd@users.noreply.github.com>
2023-03-23 00:48:16 +1100

    upgraded css, added js comments, debugging some small stuff

--- q2/src/App.js
+++ q2/src/App.js
@@ -1,23 +1,31 @@
+// Import useful modules and copmponents for this applicationn
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
+// Initializing state variable "users" with an empty array
 const App = () => {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
+    // Defining an asynchronous function "fetchUsers"
+
     const fetchUsers = async () => {
       try {
         const response = await axios.get(
+          // Fetching data from the specified URL
           "https://jsonplaceholder.typicode.com/users"
         );
+        // Setting the state variable "users" with the fetched data
         setUsers(response.data);
       } catch (error) {
+        // Logging an error message if it fails
         console.error("Error fetching users:", error);
       }
     };
 
     fetchUsers();
+    // Defining an empty dependency array to ensure the effect is only run once when the component mounts
   }, []);
 
   return (
@@ -28,16 +36,15 @@ const App = () => {
       <div className="User-list">
         {users.map((user) => (
           <div key={user.id} className="User-item">
-            <h1>{user.name}</h1>
-            <h5>&#9993;{user.email}</h5>
+            <h2>{user.name}</h2>
+            <h5>&#9993; {user.email}</h5>
+            <h5>📍 {user.address.suite}</h5>
             <h5>{user.address.street}</h5>
-            <h5>{user.address.suite}</h5>
             <h5>{user.address.city}</h5>
             <h5>{user.address.zipcode}</h5>
-            <h5>{user.phone}</h5>
-            <h5>{user.website}</h5>
-            <h5>{user.website}</h5>
-            <h5>{user.company.name}</h5>
+            <h5>📞 {user.phone}</h5>
+            <h5>🖥 {user.website}</h5>
+            <h5>🏭 {user.company.name}</h5>
             <h6>User Id:{user.id}</h6>
           </div>
         ))}
@@ -46,4 +53,5 @@ const App = () => {
   );
 };
 
+// Exporting the App component for use in other parts of the application
 export default App;
